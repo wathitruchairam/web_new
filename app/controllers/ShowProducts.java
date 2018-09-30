c824c49919feb57ee5e57641032a7d5f190dcc86
wathitruchairam <wathitruchairam@gmail.com>
2018-09-30 11:25:50 +0700

    tree update

--- app/controllers/ShowProducts.java
+++ app/controllers/ShowProducts.java
@@ -14,9 +14,19 @@ import java.util.List;
 public class ShowProducts extends Controller {
 
     public static List<Product> productList = new ArrayList<Product>();
-
+    public static Product product;
     public static Result showPro(){
         productList = Product.list();
         return Application.main(showProduct.render(productList));
+    }
+
+    public static Result listdetail(String Id){
+        int i;
+        for (i = 0; i < productList.size(); i++) {
+            if (productList.get(i).getId().equals(Id)) {
+                break;
+            }
+        }
+        return Application.main(listdetail.render(productList.get(i)));
     }
 }
